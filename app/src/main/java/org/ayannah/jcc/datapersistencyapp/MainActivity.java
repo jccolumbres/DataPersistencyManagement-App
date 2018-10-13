eba06038626823ba8bba80a7ebecb3d0b3ac3fb3
JC <jhegs.knightdev@gmail.com>
2018-10-13 23:57:10 +0800

    Set up MainActivity

--- app/src/main/java/org/ayannah/jcc/datapersistencyapp/MainActivity.java
+++ app/src/main/java/org/ayannah/jcc/datapersistencyapp/MainActivity.java
@@ -2,12 +2,18 @@ package org.ayannah.jcc.datapersistencyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvOut = findViewById(R.id.tv_out);
+
     }
 }
